d16a7254905809ea44f2ef4e8ecb6cbc6446f3bc
Harshvardhan226510 <22harshavardhan22@gmail.com>
2025-04-19 20:17:09 +0530

    Create 01_HELLO_WORLD.py

---
+++ 1-hello-world/01_HELLO_WORLD.py
@@ -0,0 +1,25 @@
+#01_HELLO_WORLD.py
+print ("hello world")
+
+#02_PATTERN.py
+print('      1')
+print('    2 3')
+print('  4 5 6')
+print('7 8 9 10')
+
+#03_INITIALS.py
+print("H   H  L")
+print("H   H  L")
+print("H   H  L")
+print("HHHHH  L")
+print("H   H  L")
+print("H   H  L")
+print("H   H  LLLLLLLL")
+
+#04_SNAILMAIL.py
+print('19-04-2025')
+
+print('I am feeling good right now ')
+print('I want to keep coding coding alive and revolutionize it ')
+print('You are goanna achieve it dont ever give up')
+print('😊')
